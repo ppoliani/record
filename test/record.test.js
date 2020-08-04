436bee27e19eb74c0a9ade3ef1414d982513afdc
ppoliani <ppoliani@gmail.com>
2020-08-04 11:01:38 +0100

    add empty test file

---
+++ test/record.test.js
@@ -0,0 +1,17 @@
+const test = require('ava');
+
+test('It should create an instance which you can then use to access the state', () => {
+
+})
+
+test('It should create an instance which you can then update the state via some function', () => {
+  
+})
+
+test('It should create multiple instances with separate state each', () => {
+  
+})
+
+test('It should reuse the functions in each instance', () => {
+  
+})
